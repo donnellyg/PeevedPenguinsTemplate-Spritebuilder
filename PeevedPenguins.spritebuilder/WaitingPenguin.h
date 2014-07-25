842f0505452e39ffbcef8b49d201afde36ac4f67
donnellyg <donnelly.gus@gmail.com>
2014-07-25 18:48:59 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,16 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Gus Donnelly on 7/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+- (void) didLoadFromCCB;
+- (void) startBlinkAndJump;
+
+@end
