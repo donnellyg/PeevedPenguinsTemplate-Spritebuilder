2eb432d3256308f3ce6322c74da55db6cab891d5
donnellyg <donnelly.gus@gmail.com>
2014-07-25 17:25:17 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -91,6 +91,7 @@
 
 - (void) ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB {
     CCLOG(@"Something collided with a seal!");
+    NSLog(@"Something collided with a seal! (NSLOG");
 }
 
 
